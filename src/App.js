a1eb84e5dbaeddd1be14e5ceb1feebe605e851d1
Suganthan <Suganthan199628@gmail.com>
2024-06-06 09:45:30 +0530

    Small changes in APp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes,Link} from 'react-router-dom';
-import Dashboard from './components/Dashboard/Dashboard';
-import Preferences from './components/Preferences/Preferences';
-import Login from './components/Login/Login';
+// import { BrowserRouter, Route, Routes,Link} from 'react-router-dom';
+// import Dashboard from './components/Dashboard/Dashboard';
+// import Preferences from './components/Preferences/Preferences';
+// import Login from './components/Login/Login';
 import ToDoList from './components/TodoList';
 import './styles.css'
 function App() {
